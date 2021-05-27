a8a6921700a98966749dcbcacf52f1fc277480a9
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-05-27 19:46:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 function fetchBooks() {
+<<<<<<< HEAD
   return fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
   .then(json => renderBooks(json));
 }
+=======
+  fetch ("https://anapioficeandfire.com/api/books")
+  .then(function(response) {
+    return response.JSON( );
+  })
+  .then(function(json) {
+    return renderBooks(json);
+  })
+>>>>>>> 08577dc83c4b6325835a4ef2d8dbac4e3e49887e
   // To pass the tests, don't forget to return your fetch!
   
 
